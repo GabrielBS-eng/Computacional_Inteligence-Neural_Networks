0e8618cc8fc2b6b7de4b76a272e0f5dd1fb267a4
GabrielEDROM <gabriel_gx7@yahoo.com.br>
2017-07-03 08:58:37 -0300

    First cmake test in this program

--- program/Main.cpp
+++ program/Main.cpp
@@ -5,17 +5,23 @@
 
 int main()
 {
-  Number Dayana = new Number();
+  Number Dayana = new Number(0);
 
   int weight[31];
+  bool array[31];
+  int i;
 
   for(short i==0; i<31; i++) //first weight values
   {
     weight[i] = DEFAULT_WEIGHT;
   }
   Dayana.setWeight(weight);
-  array = Dayana.getWeight()
-  
+  weight = Dayana.getWeight()
+  array = Dayana.getArray();
+
+  printf("\n");
+  for(i=0; i<31; i++) printf("%d ",array[i]);
+  printf("\n");
 
   return 0;
 }
