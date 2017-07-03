e051feeff727cb8871919a5a02281eebb2f4d380
GabrielEDROM <gabriel_gx7@yahoo.com.br>
2017-07-02 22:40:17 -0300

    Create main code an Dayana object

---
+++ Main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include "Number.hpp"
+
+#define DEFAULT_WEIGHT 0
+
+int main()
+{
+  Number Dayana = new Number();
+
+  int weight[31];
+
+  for(short i==0; i<31; i++) //first weight values
+  {
+    weight[i] = DEFAULT_WEIGHT;
+  }
+  Dayana.setWeight(weight);
+  array = Dayana.getWeight()
+  
+
+  return 0;
+}
