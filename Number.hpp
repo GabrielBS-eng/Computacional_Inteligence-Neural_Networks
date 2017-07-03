b8256ff2b0ab46badfb70a4f0f17afe55941b8b0
GabrielEDROM <gabriel_gx7@yahoo.com.br>
2017-07-02 21:25:49 -0300

    Number HEADER created

---
+++ Number.hpp
@@ -0,0 +1,17 @@
+#pragma once
+
+std::default_random_engine rand;
+
+class Number
+{
+  bool array[31];
+  int weight[31];
+
+public:
+  Number();
+  Number(int set);
+  void  setArray(array[]);
+  int*  getArray();
+  void  setWeight(weight[]);
+  int*  getWeight();
+}
